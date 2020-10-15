46adeee906702759974de958f3f073a574559b88
benteb <bba029@post.uit.no>
2020-10-15 18:22:57 +0200

    State and GaussianFile classes

---
+++ src/main/python/mods/GaussianFile.py
@@ -0,0 +1,40 @@
+class GaussianFile():
+    def __init__(self, file_path):
+    
+        self.file_path = file_path
+        self.job_details = {}
+
+        #TODO check convergence and analyse file
+
+    def get_filepath(self):
+        """
+        Return: filepath for gaussianfile
+        """
+        return self.file_path
+
+    def read_dft_out(self, DFT_out):
+        """ Very unfinished code, should also be moved to a GaussianOUT class
+        """
+    
+        with open(DFT_out) as f:
+            
+            for line in f:
+
+                if "Solvent" in line:
+                    self.phase = line.split()[2]
+                    self.Eps = float("{:.2f}".format(line.split()[4]))
+
+                if "SCF Done" in line:
+                    self.ene["E_gas"] = float(line.split()[4])
+                    
+                if "Zero-point correction=" in line:
+                    self.ene["ZPE"] = float(line.split()[2])
+                    
+                elif "Thermal correction to Energy= " in line:
+                    self.ene["dE"] = float(line.split()[4])
+
+                elif "Thermal correction to Enthalpy=" in line:
+                    self.ene["dH"] = float(line.split()[4])
+
+                elif "Thermal correction to Gibbs Free Energy=" in line:
+                    self.ene["dG"] = float(line.split()[6])
---
+++ src/main/python/mods/State.py
@@ -0,0 +1,54 @@
+from mods.GaussianFile import GaussianFile
+
+class State():
+
+    def __init__(self, tab_index, file_paths=None):
+
+        self.gfiles = {}
+        self.tab_index = tab_index
+
+        if file_paths:
+            self.add_gfiles(file_paths)
+
+        
+    def get_filenames(self):
+        """
+        Return: list with all filenames of current state. 
+        """
+        #return [x for x in self.gfiles.keys()] --> to use when printing filename only in workspace, instead of the whole filepath
+        return [x.get_filepath() for x in self.gfiles.values()]
+
+    def get_tab_index(self):
+        '''
+        Return: tab index associated with a given state.
+        '''
+        return self.tab_index
+
+    def add_gfiles(self, file_paths):
+        """
+        Creates GaussianFile instance for each uploaded file-path.
+        """
+
+        #if len(files_path) > some number:
+        #    do multiprocessing or threading instead? Each new GaussianFile object will undergo some processing (read file, check convergence, energies...)
+
+        for path in file_paths:
+            self.gfiles[path.split("/")[-1]] = (GaussianFile(path))
+    
+    def del_gfiles(self, files_to_del):
+        '''
+        Removes all files in files_to_del list from state.
+        '''
+
+        for f in files_to_del:
+            try:
+                self.gfiles.pop(f.split("/")[-1]) #here f is the whole path, not only the filename. 
+            except KeyError:
+                print(f"File \"{f}\" not found")
+
+    def get_all_gpaths(self):
+            '''
+            return: list of all gaussian filpaths associated with a given state.
+            '''
+
+        return [x.get_filepath() for x in self.gfiles.values()]
