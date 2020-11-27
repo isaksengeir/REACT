617f17b162d3cce2d65bfec7f2985a4769ebb69a
benteb <bba029@post.uit.no>
2020-11-27 14:59:24 +0100

    small design changes to FileEditorWindow

--- src/main/python/mods/FileEditor.py
+++ src/main/python/mods/FileEditor.py
@@ -22,23 +22,17 @@ class FileEditor(QtWidgets.QMainWindow):
             # TODO OK button will not ask for save file name, just close window
             self.save_file = False
 
-        # If file is input file, read it as it is
-        elif self.filename.split(".")[-1] in ["inp", "com"]:
+        # If file is input file, xyz or pdb, read it as it is
+        elif self.filename.split(".")[-1] in ["inp", "com", "xyz", "pdb"]:
             with open(filepath, 'r') as f:
                 text = f.read()
-            self.ui.FileEditorBox.setPlainText(text)
+            self.ui.textwindow.setPlainText(text)
 
         elif self.filename.split(".")[-1] in ["out", "log"]:
 
             self.file_content = self.react.create_input_content(self.filepath)
-            
-            self.ui.OK_button.setText('Save')
             self.setWindowTitle(self.filename.split('.')[0] + '_new.com')
-            self.ui.FileEditorBox.setPlainText(self.file_content)
-
-            self.ui.OK_button.clicked.connect(self.save_file_)
-        
-        self.ui.Cancel_button.clicked.connect(self.close)
+            self.ui.textwindow.setPlainText(self.file_content)
 
     def save_file_(self):
 
@@ -52,25 +46,5 @@ class FileEditor(QtWidgets.QMainWindow):
         
         self.react.add_files_to_list([filepath])
         self.setWindowTitle(filepath.split('/')[-1])
-
-
-    def convert_out_to_inp(self):
-        """
-        TODO: this function is good for testing, but I (Bente) think the code should be moved to a 'create_InputObject'
-        method, belonging to the State-class.
-        :return:
-        """
-        xyz = self.react.states[self.react.tabWidget.currentIndex()].get_final_xyz(self.filepath)
-        routecard = self.react.states[self.react.tabWidget.currentIndex()].get_routecard(self.filepath)
-
-        self.ui.FileEditorBox.append(routecard)
-
-        self.ui.FileEditorBox.append('\n')
-
-        for line in xyz:
-            self.ui.FileEditorBox.append(line)
-
-        
-
 
 # TODO save changes to file after editing!!! 
