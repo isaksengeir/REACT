a993985684202a30a6cdc3f952b7257d17c3269c
benteb <bba029@post.uit.no>
2021-01-27 12:32:57 +0100

    latest update on settingswindow

--- src/main/python/mods/GlobalSettings.py
+++ src/main/python/mods/GlobalSettings.py
@@ -12,21 +12,11 @@ class GlobalSettings(QtWidgets.QMainWindow):
         self.react = parent
         self.ui = Ui_SettingsWindow()
         self.ui.setupUi(self)
-
-        self.ui.add_DFT_button_1.clicked.connect(lambda: self.add_item_to_list(self.ui.additionalKeys_1, self.ui.add_DFT_list_1, "additional keys"))
-        self.ui.del_DFT_button_1.clicked.connect(lambda: self.del_item_from_list(self.ui.additionalKeys_1, self.ui.add_DFT_list_1, "additional keys"))
-        self.ui.add_DFT_button_2.clicked.connect(lambda: self.add_item_to_list(self.ui.additionalKeys_2, self.ui.add_DFT_list_2, "link 0"))
-        self.ui.del_DFT_button_2.clicked.connect(lambda: self.del_item_from_list(self.ui.additionalKeys_2, self.ui.add_DFT_list_2, "link 0"))
-        self.ui.add_DFT_button_4.clicked.connect(lambda: self.add_item_to_list(self.ui.additionalKeys_3, self.ui.add_DFT_list_3, "opt keys"))
-        self.ui.del_DFT_button_4.clicked.connect(lambda: self.del_item_from_list(self.ui.additionalKeys_3, self.ui.add_DFT_list_3, "opt keys"))
-        #self.ui.change_cwd_button.clicked.connect(self.change_cwd)
-        #self.ui.change_pymol_button.clicked.connect(self.change_pymol)
-
         
 
         # TODO: move this attribute to somewhere accessible for setup class
         self.DFT_options = {'functional': ['B3LYP', 'rB3LYP', 'M062X'],
-                            'basis set': {'3-21G': {'pol1': None, 'pol2': None, 'diff': ['+', ' ']},
+                            'basis': {'3-21G': {'pol1': None, 'pol2': None, 'diff': ['+', ' ']},
                                           '6-21G': {'pol1': ['d'], 'pol2': ['p'], 'diff': None},
                                           '4-31G': {'pol1': ['d'], 'pol2': ['p'], 'diff': None},
                                           '6-31G': {'pol1': ['d', '2d', '3d', 'df', '2df', '3df', '3d2f'],
@@ -62,9 +52,49 @@ class GlobalSettings(QtWidgets.QMainWindow):
         self.settings = parent.settings
 
         self.read_settings_set_window()
+        self.ui.add_DFT_button_1.clicked.connect(lambda: self.add_item_to_list(self.ui.additionalKeys_1, self.ui.add_DFT_list_1, "additional keys"))
+        self.ui.del_DFT_button_1.clicked.connect(lambda: self.del_item_from_list(self.ui.add_DFT_list_1, "additional keys"))
+        self.ui.add_DFT_button_2.clicked.connect(lambda: self.add_item_to_list(self.ui.additionalKeys_2, self.ui.add_DFT_list_2, "link 0"))
+        self.ui.del_DFT_button_2.clicked.connect(lambda: self.del_item_from_list(self.ui.add_DFT_list_2, "link 0"))
+        self.ui.add_DFT_button_4.clicked.connect(lambda: self.add_item_to_list(self.ui.additionalKeys_3, self.ui.add_DFT_list_3, "opt keys"))
+        self.ui.del_DFT_button_4.clicked.connect(lambda: self.del_item_from_list(self.ui.add_DFT_list_3, "opt keys"))
+        self.ui.save_button.clicked.connect(self.save_settings)
+        self.ui.cancel_button.clicked.connect(self.close)
+        self.ui.comboBox_funct.currentTextChanged.connect(lambda: self.update_settings(self.ui.comboBox_funct, "functional"))
+        self.ui.basis1_comboBox_3.currentTextChanged.connect(lambda: self.update_settings(self.ui.basis1_comboBox_3, "basis"))
+        self.ui.basis2_comboBox_4.currentTextChanged.connect(lambda: self.update_settings(self.ui.basis2_comboBox_4, "diff"))
+        self.ui.basis3_comboBox_6.currentTextChanged.connect(lambda: self.update_settings(self.ui.basis3_comboBox_6, "pol1"))
+        self.ui.basis4_comboBox_5.currentTextChanged.connect(lambda: self.update_settings(self.ui.basis4_comboBox_5, "pol2"))
+        #self.ui.change_cwd_button.clicked.connect(self.change_cwd)
+        #self.ui.change_pymol_button.clicked.connect(self.change_pymol)
+
+    def update_settings(self, widget, DFT_key):
+        """
+        :param combobox: QComboBox
+        :param DFT_key: str: key to access correct variable in self.settings
+        Updates self.settings according to update on combobox
+        """
+
+        text = widget.currentText()
+
+        if isinstance(widget, QtWidgets.QComboBox):
+            if DFT_key in ["diff", "pol1", "pol2"]:
+                self.settings["DFT"]["basis"][1][DFT_key] = text
+            else:
+                self.settings["DFT"][DFT_key] = text
+
+        elif isinstance(widget, QtWidgets.QPushButton):
+            pass
+
+
 
     def add_item_to_list(self, Qtextinput, Qlist, DFT_key):
         """
+        :param Qtextinput: QLineEdit
+        :param Qlist: QListWidget
+        :param DFT_key: str: key to access correct variable in self.settings
+        Adds the text input from user (past to Qtextinput) to correct
+        QlistWidget and updates self.settings accordingly.
         """
         user_input = Qtextinput.text()
         if user_input and user_input not in self.settings["DFT"][DFT_key]:
@@ -72,7 +102,12 @@ class GlobalSettings(QtWidgets.QMainWindow):
             Qlist.addItem(user_input)
             print(self.settings["DFT"][DFT_key])
 
-    def del_item_from_list(self, Qtextinput, Qlist, DFT_key):
+    def del_item_from_list(self, Qlist, DFT_key):
+        """
+        :param Qlist: QListWidget
+        :param DFT_key: str: key to access correct variable in self.settings
+        Removes item in QlistWdiget and updates self.settings accordingly.
+        """
         item_text = Qlist.currentItem().text()
 
         if item_text in self.settings["DFT"][DFT_key]:
@@ -82,15 +117,19 @@ class GlobalSettings(QtWidgets.QMainWindow):
 
 
     def update_basis_options(self, basis):
+        """
+        :param basis: str: name current basis
+        Updates polarization and diffuse functions avail for basis set
+        """
 
         self.ui.basis2_comboBox_4.clear()
         self.ui.basis3_comboBox_6.clear()
         self.ui.basis4_comboBox_5.clear()
 
-        if basis in self.DFT_options['basis set']:
-            basis_options = self.DFT_options['basis set'][basis]
-        elif basis in self.settings["user basis"]:
-            basis_options = self.settings["user basis"][basis]
+        if basis in self.DFT_options['basis']:
+            basis_options = self.DFT_options['basis'][basis]
+        elif basis in self.settings["DFT"]["user basis"]:
+            basis_options = self.settings["DFT"]["user basis"][basis]
         else:
             self.react.append_text('Basis set is unknown to REACT. Add polarization and diffuse functions as needed.')
             return
@@ -115,7 +154,7 @@ class GlobalSettings(QtWidgets.QMainWindow):
         # fill functional and basis set comboboxes
         self.ui.comboBox_funct.addItems(self.DFT_options['functional'])
         self.ui.comboBox_funct.addItems(self.settings["DFT"]["user funct"])
-        self.ui.basis1_comboBox_3.addItems([x for x in self.DFT_options['basis set']])
+        self.ui.basis1_comboBox_3.addItems([x for x in self.DFT_options['basis']])
         self.ui.basis1_comboBox_3.addItems([x for x in self.settings["DFT"]["user basis"]])
 
         # Read current settings, set window accordingly
@@ -149,18 +188,17 @@ class GlobalSettings(QtWidgets.QMainWindow):
                     self.ui.dark_button.setChecked(True)
                 else:
                     self.ui.light_button.setChecked(True)
+
+    def save_settings(self):
+        self.react.settings = self. settings
+        self.close()
+
     
     def switch_Ui_colormode(self, color):
         """
         Switch Ui colormode. Darkmode: color = 1, lightmode: color = 0 
         """
         pass
-
-
-    def add_keywords(self, keys, list):
-        #
-        pass
-
 
 
     def closeEvent(self, event):
